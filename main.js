d26a034be82b6b0368e82e30a11cdff710af1226
Lucas Henrique Coelho <lucas.h.coelho20@gmail.com>
2021-11-17 18:09:59 -0300

    Adicionado JS

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-const elemento lista
+const elementoLista = document.querySelector('ul');
+const elementoInput = document.querySelector('input');
+const elementoBotao = document.querySelector('button');
+
+const tarefas = [
+    
+]
+
+function mostrarTarefas() {
+
+    elementoLista.innerHTML = ''
+
+    for (tarefa of tarefas) {
+        const elementoTarefa = document.createElement('li')
+        const textoTarefa = document.createTextNode(tarefa)
+
+        elementoTarefa.appendChild(textoTarefa)
+        elementoLista.appendChild(elementoTarefa)
+    }
+}
+
+mostrarTarefas()
+
+function addTarefas() {
+
+    const textoTarefa = elementoInput.value
+    tarefas.push(textoTarefa)
+    elementoInput.value =''
+
+    mostrarTarefas()
+}
+
+elementoBotao.setAttribute('onclick', 'addTarefas()')
