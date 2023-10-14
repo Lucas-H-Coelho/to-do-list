c0d032d718be244945e12f2681ed9227ef47f4a4
Lucas Henrique Coelho <35717565+Lucas-H-Coelho@users.noreply.github.com>
2023-10-14 16:45:34 -0300

    Corrigido aparência

--- main.js
+++ main.js
@@ -1,48 +1,44 @@
-const elementoLista = document.querySelector('ol');
-const elementoInput = document.querySelector('input');
-const elementoBotao = document.querySelector('button');
+const elementoLista = document.querySelector("ol");
+const elementoInput = document.querySelector("input");
+const elementoBotao = document.querySelector("button");
 
-const tarefas = [
-    
-]
+const tarefas = [];
 
 function mostrarTarefas() {
+  elementoLista.innerHTML = "";
 
-    elementoLista.innerHTML = ''
+  for (tarefa of tarefas) {
+    const elementoTarefa = document.createElement("li");
+    const textoTarefa = document.createTextNode(tarefa);
 
-    for (tarefa of tarefas) {
-        const elementoTarefa = document.createElement('li')
-        const textoTarefa = document.createTextNode(tarefa)
-        
-        const elementoLink = document.createElement('a')
-        const pos = tarefas.indexOf(tarefa)
+    const elementoLink = document.createElement("a");
+    const pos = tarefas.indexOf(tarefa);
 
-        const linkText = document.createTextNode('X')
-        elementoLink.appendChild(linkText)
+    const linkText = document.createTextNode("x");
+    elementoLink.appendChild(linkText);
 
-        elementoLink.setAttribute('href', '#')
-        elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`)
+    elementoLink.setAttribute("href", "#");
+    elementoLink.setAttribute("onclick", `deletaTarefa(${pos})`);
 
-        elementoTarefa.appendChild(textoTarefa)
-        elementoLista.appendChild(elementoTarefa)
-        elementoTarefa.appendChild(elementoLink)
-    }
+    elementoTarefa.appendChild(textoTarefa);
+    elementoLista.appendChild(elementoTarefa);
+    elementoTarefa.appendChild(elementoLink);
+  }
 }
 
-mostrarTarefas()
+mostrarTarefas();
 
 function addTarefas() {
+  const textoTarefa = elementoInput.value;
+  tarefas.push(textoTarefa);
+  elementoInput.value = "";
 
-    const textoTarefa = elementoInput.value
-    tarefas.push(textoTarefa)
-    elementoInput.value =''
-
-    mostrarTarefas()
+  mostrarTarefas();
 }
 
-elementoBotao.setAttribute('onclick', 'addTarefas()')
+elementoBotao.setAttribute("onclick", "addTarefas()");
 
 function deletaTarefa(pos) {
-    tarefas.splice(pos, 1)
-    mostrarTarefas()
+  tarefas.splice(pos, 1);
+  mostrarTarefas();
 }
